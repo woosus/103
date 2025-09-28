ef48e66976bafd8e0715ceadceca4a5e0c68590b
woosus <idspoi7@gmail.com>
2025-09-28 21:16:57 +0700

    Add files via upload

---
+++ config_fixed.js
@@ -0,0 +1,20 @@
+window.BET33_CONFIG = {
+  JOIN_URL: "@@@@@JOIN_URL@@@@@",
+  KAKAO_LINK: "@@@@@KAKAO_LINK@@@@@",
+  TELEGRAM_LINK: "@@@@@TELEGRAM_LINK@@@@@",
+  LOGO_SRC: "assets/logo.png",
+
+  VIDEO_COUPON_APPLY: "쿠폰신청하기.mp4",
+  VIDEO_EMERGENCY: "긴급쿠폰신청.MP4",
+  VIDEO_PROMO: "프로모션.mp4",
+
+  PROMO_SLIDES: [
+    { top: "assets/banners/p1.png",  bottom: "assets/banners/P1-1.png" },
+    { top: "assets/banners/p2.png",  bottom: "assets/banners/p2-1.png" },
+    { top: "assets/banners/p3.png",  bottom: "assets/banners/p3-3.png" },
+  ],
+
+  PROMO_MODES: ["slide","collapse"],
+  PROMO_BG_TIMING: "with",
+  PROMO_INTERVAL_SEC: 4,
+};
